3a81496e1c7d01088425dc1977cfc98dc4665048
Gregory Keeley <gregkeeley@Gregorys-MBP.fios-router.home>
2019-10-03 23:46:27 -0400

    v.04- Added additional requirement for lab criteria, addded user info section, added timing for readibility

--- PathAdventure/main.swift
+++ PathAdventure/main.swift
@@ -8,12 +8,41 @@
 
 import Foundation
 
+let commandSymbol = "\u{2318}" //Setting a symbol for use later
+
+//======================================
+// Setting up user Information
+//======================================
+print("What is your name?")
+var userName = (readLine())
+print("What is your favorite breakfast food?")
+var favBreakfast = (readLine())
+print("Who is your favorite music artist?")
+var favArtist = (readLine())
+
+var userInfo = (name: userName,breakfast: favBreakfast,music: favArtist)
+
 //================================================================================================
 //===========Path Selection=======================================================================
 //================================================================================================
+sleep(1)
 
+print("You are sound asleep when you hear your alarm ringing. Do you hit the snooze button, yes or no?")
+var snoozePress:String = (readLine()?.lowercased() ?? " ")
+
+if snoozePress == ("yes") {
+    for snoozePress in 1...3 {
+        sleep(1);print("You pressed snooze \(snoozePress) times!")
+    }
+} else if snoozePress == ("no") {
+    print("Great job \(String(describing: userInfo.name))! you didn't hit the snooze button! But you had a 3 course including \(String( describing: userInfo.breakfast)) breakfast that took way too long to make.")
+}
+
+sleep(1)
 print("""
-Running late for class at 9:10am, you have three options to get to Pursuit.
+
+
+Running late for class now, you have 4 options to get to Pursuit.
 
 1: Subway
 2: Bus
@@ -22,7 +51,7 @@ Running late for class at 9:10am, you have three options to get to Pursuit.
 
 Which do you choose?
 """)
-var pathNum = Int(readLine() ?? "-1") ?? -1
+let pathNum = Int(readLine() ?? "-1") ?? -1
 
 //================================================================================================
 //===========Subway Path==========================================================================
@@ -30,6 +59,7 @@ var pathNum = Int(readLine() ?? "-1") ?? -1
 
 if pathNum == 1 {
     print("""
+
 You walk 10 minutes to the subway, and swipe in. The local is about to arrive, and the express is not far behind.
 Do you take the local or the express?
 
@@ -38,21 +68,27 @@ Do you take the local or the express?
 """)
 }
 
-
 let subwayPath1 = Int(readLine() ?? "-1") ?? -1
+
 
 if subwayPath1 == 1 {
     print("""
-You get on the local train, put on some music and cross your fingers the train will arrive on time.
+
+        You get on the local train, and start listening to \(String(describing: userInfo.music)) and cross your fingers the train will arrive on time.
 With seconds to spare, you run to the class, sweaty but on time. Congratulations! It is time to code.
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 } else if subwayPath1 == 2 {
     print("""
+
 You wait for the express and watch the local leave the station. The train rolls in, you hop on and the doors close.
 Halfway to the express stop, the train comes to a sudden halt. You hear an annoucement "Ladies and gentleman, due to
 some unruly teenagers pulling the emergency brakes, we are cannot move until we fix the brakes. Sorry."
 You are late to class after a 3 hour delay.
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
 
 //================================================================================================
@@ -61,6 +97,7 @@ You are late to class after a 3 hour delay.
 
 if pathNum == 2 {
     print("""
+
 You walk 5 minutes to the bus, and see no bus in sight. You can wait, or you can continue walking to the subway.
 Do you wait, or walk to the subway?
 
@@ -70,30 +107,62 @@ Do you wait, or walk to the subway?
 }
 
 
-
 let busPath = Int(readLine() ?? "-1" ) ?? -1
-
 var busTime = Int.random(in: 1...70)
-
 
 if busPath == 1 {
     switch busTime {
     case 1...15:
         print("It took the bus \(busTime) minutes to arrive. You arrive in time to learn some coding!")
+        print(" ")
+        sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
     case 15...30:
         print("It took the bus \(busTime) minutes to arrive. You make it to class with seconds to spare!")
+        print(" ")
+        sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
     case 30...60:
         print("It took the bus \(busTime) minutes to arrive. You are late to class, but you arrived, eager to learn.")
+        print(" ")
+        sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
     default:
-        print("It has taken over an hour (\(busTime)) for the bus to arrive, and you are late for class. Try again tomorrow.")
+        print("It has taken (\(busTime)) minutes for the bus to arrive, and you are late for class.")
+        print(" ")
+        sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
     }
 } else if busPath == 2 {
     print("""
-You decide to walk to the subway.
+
+You decide to walk to the subway.You walk 10 minutes to the subway, and swipe in. The local is about to arrive, and the express is not far behind.
+Do you take the local or the express?
+
+1: Local
+2: Express
 """)
 }
 
-// **Finish this narration to point to the pathNum1**
+let subwayPath2 = Int(readLine() ?? "-1") ?? -1
+
+
+if subwayPath2 == 1 {
+    print("""
+
+You get on the local train, put on some music and cross your fingers the train will arrive on time.
+With seconds to spare, you run to the class, sweaty but on time. Congratulations! It is time to code.
+""")
+    print(" ")
+sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
+} else if subwayPath2 == 2 {
+    print("""
+
+You wait for the express and watch the local leave the station. The train rolls in, you hop on and the doors close.
+Halfway to the express stop, the train comes to a sudden halt. You hear an annoucement "Ladies and gentleman, due to
+some unruly teenagers pulling the emergency brakes, we are cannot move until we fix the brakes. Sorry."
+You are late to class after a 3 hour delay.
+""")
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
+}
+
 
 //================================================================================================
 //===========Walk Path============================================================================
@@ -101,6 +170,7 @@ You decide to walk to the subway.
 
 if pathNum == 3 {
     print("""
+
 You decide to walk to school. Along the way, you encounter a homeless man asking for .87 cents for gas so he can
 get back home to his sick kids in New Jersey. What do you do?
 
@@ -113,19 +183,25 @@ let walkPath = Int(readLine() ?? "-1" ) ?? -1
 
 if walkPath == 1 {
     print("""
+
 Shrugging off the desperate plea of this sketchy guy, you continue to walk to class.
 Looking over your shoulder you notice he is now following you.
 After several blocks with him following you, you finally make it to Pursuit and
 the protection of your fellows. As a group, you scare him off with your newly branded
 programming skills, and you all enjoy a nice day of coding!
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 } else if walkPath == 2 {
     print("""
+
 After giving the sketchy man a dollar and asking for change, you both laugh hysterically and begin a nice conversation.
 He tells his life story and you discover you have much in common. As he walked with you to class, you both decide to
 to spend some time together and you skip class and meet his family. Arriving in New Jersey hours later, you expect to
 meet his family when you enter his home. Hours later you wake up and discover your organs have been harvested.
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
 
 //================================================================
@@ -134,6 +210,7 @@ meet his family when you enter his home. Hours later you wake up and discover yo
 
 if pathNum == 4 {
     print("""
+
 You decided to ride your bike to Pursuit and get a little bit of excercise while you're at it!
 Which route do you take?
 
@@ -154,6 +231,7 @@ var debrisChoice = Int(readLine() ?? "-1") ?? -1
 
 if bikePath == 1 {
     print("""
+
 You get on your bike and starting pedaling towards Queens Blvd. Leisurely rolling towards Pursuit, you bypass
 much of the traffic on your way. When you are only several blocks away, you notice at the last second, there
 is debris in the road. Thinking quickly, you attempt to swerve around the debris. Do you go left or right?
@@ -161,27 +239,34 @@ is debris in the road. Thinking quickly, you attempt to swerve around the debris
 1: to the left
 2: to the right
 """
-)
+    )
 }
 if debrisChoice == 1 { // Left of the debris
     print("""
+
 You quickly attempt to swerve to the left of the debris and somehow manage to avoid each screw, nail and splinter!
 The rest of the ride goes smoothly, you chain up your bike, and enjoy a great day of coding at Pursuit!
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
 if debrisChoice == 2 {
     print("""
+
 You quick attempt to swerve to the right of the debris and somehow manage to avoid all of the dangerous garbage in your way!
 But in your haste, you swerved into traffic and get clipped by a bus. You remain unharmed somehow, but your bike is ruined
 and lays in pieces all of over Queens Blvd. Shook, you go home and stare into the mirror as you question your decision making skills.
 """)
+    print(" ")
+sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
 
 //================Bike path 2========================================================================================
 
 
 if bikePath == 2 {
-print("""
+    print("""
+
 You get on your bike and starting pedaling towards Pursuit on 47th Ave. It's a relatively easy ride until you get within
 a few blocks of Pursuit, when you get to the cement facility. With trucks loading up and lots of activity on both sides of the road
 you can wait, or you can go around them. Do you wait or go around?
@@ -196,15 +281,30 @@ var cementChoice = Int(readLine() ?? "-1") ?? -1 // Does this need to be in this
 
 if cementChoice == 1 {
     print("""
+
 You decide to wait for the trucks to sort themselves out as they head out to whatever work site. After a few moments, there is a clear
 path for you to continue and you roll through, unimpeded, making it to class on time. Time to code!
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
 if cementChoice == 2 {
     print("""
+
 You deccide to try and go around the cement trucks, and in the confusion they never see you. Busy as they are, they continue their work
 and through a weird Rube Goldberg like series of events, you somehow manage inside the mixing drum, full of wet cement. Unable to escape or
 cry out for help, you end up being buried in the foundation of one the newest buildings in Queen Plaza. Discoverd centuries later by future
 achreologists along with your mangled bike.
 """)
+    print(" ")
+    sleep(2);print("Press \(commandSymbol) + R to do this again tomorrow!")
 }
+
+
+
+//=====================================================
+// Win / Lose Statements
+//=====================================================
+
+
+
--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Greg Keeley
 
 --0.03 Text Adventure--
 
+v.04 - Added additional criteria for lab requirements. Added user info section. Added timing for readiblity
+
 v.03 - Added bike path, and finished narration for some paths
 
 v.02 - Updated paths with narration options.
